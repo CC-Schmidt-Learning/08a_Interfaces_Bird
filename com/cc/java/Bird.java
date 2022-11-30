7061f3f87a80213c069636179642a62fb87347f3
Daniela <Daniela-Haeussler@gmx.de>
2022-11-30 10:47:15 +0100

    override

--- com/cc/java/Bird.java
+++ com/cc/java/Bird.java
@@ -1,10 +1,21 @@
 package com.cc.java;
 
-public class Bird implements Flyable{
+public class Bird implements Flyable, IFeathers{
 
     @Override
     public String fly() {
-        return "I'm a bird, I can fly";
+        return "Blackbird fly" +","+ 
+        "Into the light of the dark black night";
     }
+
+    @Override
+    public String hasfeathers() {
+        return "I've a lot of feathers";
+    }
+
+    // @Override
+    // public String fly() {
+    //     return "I'm a bird, I can fly";
+    // }
     
 }
